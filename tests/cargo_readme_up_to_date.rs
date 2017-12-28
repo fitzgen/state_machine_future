d682d3174f7b9e64e6029abcdf6c081f697fce3f
hcpl <hcpl.prog@gmail.com>
2017-12-29 01:43:44 +0200

    Fix `cargo readme` detection in tests

--- tests/cargo_readme_up_to_date.rs
+++ tests/cargo_readme_up_to_date.rs
@@ -6,13 +6,14 @@ use std::process::Command;
 fn cargo_readme_up_to_date() {
     println!("Checking that `cargo readme > README.md` is up to date...");
 
-    let expected = Command::new("cargo")
+    let output = Command::new("cargo")
         .arg("readme")
         .current_dir(env!("CARGO_MANIFEST_DIR"))
         .output()
-        .expect("should run `cargo readme` OK")
-        .stdout;
-    let expected = String::from_utf8_lossy(&expected);
+        .expect("should run `cargo readme` OK");
+
+    assert!(output.status.success(), "Check if you have `cargo-readme` in $PATH");
+    let expected = String::from_utf8_lossy(&output.stdout);
 
     let actual = {
         let mut file = File::open(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))
